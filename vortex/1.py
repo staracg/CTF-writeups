0eab03f0ee2aa2727c2c1068219e7fbda736e169
Ayumi <staracg@gmail.com>
2017-03-24 18:50:41 +0800

    Create 1.py

---
+++ vortex/1.py
@@ -0,0 +1,24 @@
+import socket
+import struct
+import telnetlib
+from hexdump import hexdump
+
+def q(a):
+   return struct.pack("I", a)
+
+def interact():
+   print " ** INTERACT ** "
+   t = telnetlib.Telnet()
+   t.sock = s
+   t.interact()
+
+s = socket.create_connection(("vortex.labs.overthewire.org", 5842))
+
+acc = 0
+
+for i in range(4):
+   acc += struct.unpack("I", s.recv(4))[0]
+
+s.send(q(acc))
+
+interact()
